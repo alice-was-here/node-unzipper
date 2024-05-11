c80fd62cd8d387da247ea6b1802c34ae14ee838e
Ziggy Jonsson <ziggy.jonsson.nyc@gmail.com>
2024-05-11 14:38:51 -0400

    Retry stream if not big enough

--- test/office-files.js
+++ test/office-files.js
@@ -2,8 +2,16 @@ const test = require('tap').test;
 const path = require('path');
 const unzip = require('../');
 
-test("get content a docx file without errors", async function () {
-  const archive = path.join(__dirname, '../testData/office/testfile.docx');
+var test = require('tap').test;
+var fs = require('fs');
+var path = require('path');
+var unzip = require('../');
+var il = require('iconv-lite');
+var Promise = require('bluebird');
+var NoopStream = require('../lib/NoopStream');
+
+test("get content a docx file without errors", async function (t) {
+  var archive = path.join(__dirname, '../testData/office/testfile.docx');
 
   const directory = await unzip.Open.file(archive);
   await Promise.all(directory.files.map(file => file.buffer()));
@@ -14,4 +22,18 @@ test("get content a xlsx file without errors", async function () {
 
   const directory = await unzip.Open.file(archive);
   await Promise.all(directory.files.map(file => file.buffer()));
+});
+
+test("stream retries when the local file header indicates bigger size than central directory", async function (t) {
+  var archive = path.join(__dirname, '../testData/office/testfile.xlsx');
+  let retries = 0, size;
+  const directory = await unzip.Open.file(archive, {padding: 10});
+  const stream = directory.files[0].stream();
+  stream.on('streamRetry', _size => {
+    retries += 1;
+    size = _size;
+  });
+  await new Promise(resolve => stream.pipe(NoopStream()).on('finish', resolve));
+  t.ok(retries === 1, 'retries once');
+  t.ok(size > 0, 'size is set');
 });
